23329e9708514d737cdc15243fa10512ac053d3c
James Aaron Erang <iaacornus.devel@gmail.com>
2023-10-17 17:51:36 +0800

    installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ ! (command -v conda) ]]:
+function conda_install() {
+    if [[ ! $(command -v conda &> /dev/null) ]]; then
+        wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
+        bash Anaconda3-2023.09-0-Linux-x86_64.sh
+    end
+}
+
+conda_install
+conda update conda
+conda create -n prototype python=3.11 anaconda
+conda activate prototype
+
+
+
